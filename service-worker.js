3abed978fa2975328b5ff7544ef5dcb085f2aaeb
Ned06 <nedezechiel@gmail.com>
2026-04-13 11:45:48 +0000

    pass-repas

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'quickeat-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://via.placeholder.com/192'
+];
+
+// Installation : Mise en cache des fichiers de base
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cache ouvert');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activation : Nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Stratégie Fetch : Répondre avec le cache si hors-ligne
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
